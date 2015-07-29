c64e5a2f6103ada4ac086d52258174780576b66e
Yoshinobu Wakamatsu <yoshinobu_wk@volare.jp>
2015-07-29 19:23:13 +0900

    add controller test

---
+++ test/controllers/posts_controller.scala
@@ -0,0 +1,23 @@
+import play.api._
+import play.api.mvc._
+import org.scalatest._
+import play.api.test._
+import play.api.test.Helpers._
+import org.scalatestplus.play._
+import play.api.data._
+import play.api.data.Forms._
+import controllers.Posts
+
+import models.Post
+
+class PostsControllerSpec extends PlaySpec {
+  val controller = new Posts
+
+  "index action" must {
+    "return 200" in {
+      val result = controller.index().apply(FakeRequest())
+      status(result) mustBe 200
+    }
+  }
+}
+
