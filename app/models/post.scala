e0a6e4f104e12b5828564120ffb063feb148860b
Yoshinobu Wakamatsu <yoshinobu_wk@volare.jp>
2015-07-29 19:18:12 +0900

    add model methods

--- app/models/post.scala
+++ app/models/post.scala
@@ -5,3 +5,12 @@ case class Post(title: String, content: String) {
   require(content != null && !content.isEmpty, "Post must have a content")
 }
 
+object Post {
+  private var posts = Set(
+      Post("Cats", "Cats are nice"),
+      Post("Elephants", "Elephants are too big")
+    )
+
+  def add(post: Post) = (posts = posts + post)
+  def findAll = posts.toList
+}
